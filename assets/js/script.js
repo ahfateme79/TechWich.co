a3179f9770a9d9cc303b53f37402657e362bb618
GeeksforGeeks <ahfateme79@gmail.com>
2022-10-23 09:38:41 -0700

    some edit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let bar = document.querySelectorAll('.bar')
+let persentage = document.querySelectorAll('.persentage')
+let progress_child=document.querySelectorAll('.progress-child')
+
+
+for (let i = 0; i < bar.length; i++) {
+    bar[i].style.width = persentage[i].innerHTML 
+    persentage[i].style.left=persentage[i].innerHTML
+}
+
+for (let i = 0; i < progress_child.length; i++) {
+    progress_child[i].style.width = persentage[i].innerHTML 
+    persentage[i].style.left=persentage[i].innerHTML
+}
