4a2ad32a8626b6afc84a5fa02ebdfd031b307ebe
fateme ahmadi <ahfateme79@gmail.com>
2023-01-04 22:01:59 -0800

    commit

---
+++ assets/js/hour.js
@@ -0,0 +1,37 @@
+let dates=document.querySelectorAll(".off-time p")
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 12, 2023 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+
+  for (let i = 0; i < dates.length; i++) {
+    dates[i].innerHTML=days + " : " + hours + " : "
+    + minutes + " : " + seconds;
+  
+  }
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    for (let i = 0; i < dates.length; i++) {
+        dates[i].innerHTML== "EXPIRED";
+      
+      }
+  }
+}, 1000);
